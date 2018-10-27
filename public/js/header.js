e23518725bc595c7146c7726248fe66a07d64f4d
runoob <test@runoob.com>
2018-10-27 17:02:22 +0800

    10.27

--- public/js/header.js
+++ public/js/header.js
@@ -2,6 +2,7 @@
 define(["jquery"],function($){
 	function Header(){
 		this.load();
+		this.genCode();
 	}
 	
 	Header.prototype={
@@ -15,6 +16,57 @@ define(["jquery"],function($){
 		headerHandler:function(data){
 			//console.log(data);
 			$("header").html(data);
+			var cc=window.location.href;
+//			console.log(cc);
+			if (cc=="http://localhost:3000/html/position.html"){
+				$(".collapse>ul:first li:eq(0)").removeClass("active");
+				$(".collapse>ul:first li:eq(1)").addClass("active");
+			}
+			//注册事件监听
+			this.addListener();
+			//获取用户信息
+			this.loginUsers();
+		},
+		addListener(){
+			$(".btn-login").on("click",this.loginHandler);
+			$(".logout").on("click",this.loginOut);
+		},
+		loginHandler(){
+			var data=$(".form-login").serialize();
+//			console.log(data);
+			const url="http://rap2api.taobao.org/app/mock/115326/api/users/login";
+			$.post(url,data,(data)=>{
+				if (data.res_body.status===1){
+					sessionStorage.username=data.res_body.data.username;
+					//刷新页面
+					location.reload();
+				}else{
+					$(".login-error").removeClass("hidden");
+				}
+			},"json");
+		},
+		//读取username中是否存在用户
+		loginUsers(){
+			const users=sessionStorage.username;
+			if (users){
+				$(".login-success").removeClass("hidden").prev("ul").remove();
+				$(".login-success a:first").text("欢迎："+users);
+			}
+		},
+		//用户注销
+		loginOut(){
+			sessionStorage.removeItem("username");
+			$.getJSON("http://rap2api.taobao.org/app/mock/115326/api/users/logout",(data)=>{
+				if(data.res_body.status===1){
+					location.reload();
+				}
+			});
+		},
+		//生成验证码
+		genCode(){
+			$.getJSON("/api/captcha",(data)=>{
+				$(".code").html(data.res_body.data);
+			});
 		}
 	}
 	
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
-
+//svg验证码
+var svgCaptcha = require('svg-captcha');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+//生成验证码
+router.get("/api/captcha",function(req,res,next){
+	var captcha = svgCaptcha.create({color:true});// {data: '<svg.../svg>', text: 'abcd'}
+    req.session.captcha = captcha.text;
+    //将验证码<svg>标签返回浏览器
+    res.status(200).json({
+    	res_code:1,
+    	res_error:"",
+    	res_body:{
+    		data:captcha.data
+    	}
+    });
+});
+//验证验证码
+router.get("/api/captcha/verify",function(req,res,next){
+	//获取请求中传递到服务器的验证码字符串
+	const {code}=req.query;
+	//比较验证码是否输入正确
+	var valid;
+	if(code.toUpperCase()===req.session.captcha.toUpperCase()){
+		valid=true;
+	}else{
+		valid=false;
+	}
+	
+	res.json({
+		res_code:1,
+		res_error:"",
+		res_body:{
+			valid//对象简写的方式
+		}
+	});
 });
 
 module.exports = router;
